93900d3a2fd457f3286d6dbd11e8508b79f02d4d
arverma007 <aman@sigmoidanalytics.com>
2020-10-19 11:27:53 +0530

    add improvements, #1

--- GUI/frames.py
+++ GUI/frames.py
@@ -3,7 +3,7 @@ def input_frame(root, tk, cmd):
     frame_top = tk.Frame(root, bg='#22e5d5')
     frame_top.place(relx=0, rely=0, relwidth=1, relheight=0.3)
 
-    input_field = tk.Entry(frame_top, bg='#8cfff1')
+    input_field = tk.Entry(frame_top, textvariable="Click PlayNext to start", bg='#8cfff1')
     input_field.place(relx=0.1, rely=0.5, relwidth=0.55, relheight=0.3)
 
     button_check = tk.Button(frame_top, text="Check", command=lambda: cmd.validate_result(input_field.get()))
@@ -18,7 +18,10 @@ def input_frame(root, tk, cmd):
     frame_bottum = tk.Frame(root, bg='#8cfff1')
     frame_bottum.place(relx=0, rely=0.6, relwidth=1, relheight=0.4)
 
-    meaning = tk.Label(frame_bottum, bg='#affff4', wraplength=350, justify="center")
+    rules = "1. Click <PlayNext> to start the game\n2. Enter the word in the input\n3. Click <Check> for " \
+            "result\n4. Click <Replay> to reply\n5. Click <meaning> to get the word's definition"
+
+    meaning = tk.Label(frame_bottum, text=rules, bg='#affff4', wraplength=350, justify="left")
     meaning.place(relx=0.1, rely=0, relwidth=0.8, relheight=0.8)
 
     button_meaning = tk.Button(frame_buttum, text="Meaning", command=lambda: cmd.find_meaning(meaning))
